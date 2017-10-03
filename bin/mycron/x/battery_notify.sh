e6b7c45c04d3b7f040f9601f61eaf28610c46600
Simone Perriello <tigerjack89@gmail.com>
2017-10-03 15:53:34 +0200

    Small changes
    Signed-off-by: Simone Perriello <tigerjack89@gmail.com>

--- bin/mycron/x/battery_notify.sh
+++ bin/mycron/x/battery_notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Battery ranges
-VHIHG_BATTERY="60"
+VHIGH_BATTERY="60"
 HIGH_BATTERY="40"
 LOW_BATTERY="20"
 VLOW_BATTERY="10"
@@ -34,22 +34,22 @@ while [ true ]; do
 		    echo "Fujii $CURRENT_BATTERY_LEVEL $CURRENT_BATTERY_STATUS"
     		    notify-send -u "critical" -i "battery-low" -c "device" "Bat ${CURRENT_BATTERY_LEVEL}%!"
 		fi
-		sleep $SLEEP_CRITICAL
+		sleep $SLEEP_VLOW
 	elif [ $CURRENT_BATTERY_LEVEL -le $LOW_BATTERY ]; then
 	    if [ "$CURRENT_BATTERY_STATUS" == "Discharging" ]; then
     		notify-send -u "critical" -i "battery-low" -c "device" "Bat ${CURRENT_BATTERY_LEVEL}%!"
 	    fi
 	    echo "$(date)"
 	    echo "Accuort $CURRENT_BATTERY_LEVEL $CURRENT_BATTERY_STATUS"
-	    sleep $SLEEP_VLOW
+	    sleep $SLEEP_LOW
 	elif [ $CURRENT_BATTERY_LEVEL -le $HIGH_BATTERY ]; then
 		echo "$(date)"
 		echo "Ttt appo' $CURRENT_BATTERY_LEVEL $CURRENT_BATTERY_STATUS"
-		sleep $SLEEP_LOW
-	elif [ $CURRENT_BATTERY_LEVEL -le $VHIHG_BATTERY ]; then
+		sleep $SLEEP_HIGH
+	elif [ $CURRENT_BATTERY_LEVEL -le $VHIGH_BATTERY ]; then
 		echo "$(date)"
 		echo "Fin e munn' $CURRENT_BATTERY_LEVEL $CURRENT_BATTERY_STATUS"
-		sleep $SLEEP_HIGH
+		sleep $SLEEP_VHIGH
 	else
 		echo "$(date)"
 	        echo "E che miserj" $CURRENT_BATTERY_LEVEL $CURRENT_BATTERY_STATUS
