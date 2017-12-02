517c9e760c90bdb4109585922f83a61ce0be4309
Simone Perriello <tigerjack89@gmail.com>
2017-12-02 22:04:39 +0100

    pdf_xchange_wrapper: revert to version 6
    Signed-off-by: Simone Perriello <tigerjack89@gmail.com>

--- bin/pdf_xchange_wrapper.sh
+++ bin/pdf_xchange_wrapper.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+#env WINEPREFIX=/mnt/internal/LinuxData/wine/64/ /usr/bin/wine /mnt/internal/LinuxData/wine/64/drive_c/Program\ Files\ \(x86\)/Tracker\ Software/PDF\ Editor/PDFXEdit.exe "$1"
 env WINEPREFIX=/mnt/internal/LinuxData/wine/64/ /usr/bin/wine /mnt/internal/FastFiles/ProgrammiStandalone/Windows/Reader/PDF-XChange\ Editor\ Plus\ \[6.0.317.1\]\ \[crack\]/PDFXEdit.exe "$1"
 #env WINEPREFIX=/mnt/internal/LinuxData/wine/64/ /usr/bin/wine '/mnt/internal/FastFiles/ProgrammiStandalone/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe' "$@"
 
