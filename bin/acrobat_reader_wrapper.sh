61f7203862c340aa08d93691dc8087f74b6f6947
Simone Perriello <tigerjack89@gmail.com>
2018-01-04 12:38:59 +0100

    bin: changed path in acrobat reader and pdfxchange wrapper

--- bin/acrobat_reader_wrapper.sh
+++ bin/acrobat_reader_wrapper.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-env WINEARCH=win32 env WINEPREFIX=/mnt/internal/LinuxData/wine/32/ wine '/mnt/internal/LinuxData/wine/32/drive_c/Program Files/Adobe/Acrobat Reader DC/Reader/AcroRd32.exe' "$@"
+env WINEARCH=win32 env WINEPREFIX=/mnt/internal/LinuxData/wine/q4_32/ wine '/mnt/internal/LinuxData/wine/q4_32/drive_c/Program Files/Adobe/Acrobat Reader DC/Reader/AcroRd32.exe' "$@"
