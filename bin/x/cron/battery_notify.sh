3c4735247095c5a83940d6744168eb1deec756bb
Simone Perriello <simone.perriello@protonmail.com>
2018-03-10 19:04:13 +0100

    battery_notify: fixed bugs and added send_message as a function
    Signed-off-by: Simone Perriello <simone.perriello@protonmail.com>

--- bin/x/cron/battery_notify.sh
+++ bin/x/cron/battery_notify.sh
@@ -28,25 +28,34 @@ function critical_action() {
 		fi
 }
 
+function send_message() {
+    if [ -n "$DISPLAY" ]; then
+		    notify-send -u "critical" -i "battery-low" -c "device" "$1"
+    else
+        #The else part is not used ever ATM
+        wall "$1"
+    fi
+}
+
 while [ true ]; do
 	  CURRENT_BATTERY_LEVEL=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 	  CURRENT_BATTERY_STATUS=$(cat /sys/class/power_supply/"$BAT"/status)
     SLEEP_TIME=$SLEEP_VLOW
 	  if [ $CURRENT_BATTERY_LEVEL -le $CRITICAL_BATTERY ]; then
-        SLEEP_TIME=$SLEEP_CRITICAL
 		    if [ "$CURRENT_BATTERY_STATUS" == "Discharging" ]; then
             critical_action
 		    fi
+        SLEEP_TIME=$SLEEP_CRITICAL
 	  elif [ $CURRENT_BATTERY_LEVEL -le $VLOW_BATTERY ]; then
-		    SLEEP_TIME=$SLEEP_VLOW
 		    if [ "$CURRENT_BATTERY_STATUS" == "Discharging" ]; then
-            NOTIFY_MSG="Bat ${CURRENT_BATTERY_LEVEL}%!"
+            NOTIFY_MSG="Bat $CURRENT_BATTERY_LEVEL%!"
 		    fi
+		    SLEEP_TIME=$SLEEP_VLOW
 	  elif [ $CURRENT_BATTERY_LEVEL -le $LOW_BATTERY ]; then
-	      SLEEP_TIME=$SLEEP_LOW
 	      if [ "$CURRENT_BATTERY_STATUS" == "Discharging" ]; then
-            NOTIFY_MSG="Bat ${CURRENT_BATTERY_LEVEL}%!"
+            NOTIFY_MSG="Bat $CURRENT_BATTERY_LEVEL%!"
 	      fi
+	      SLEEP_TIME=$SLEEP_LOW
 	  elif [ $CURRENT_BATTERY_LEVEL -le $HIGH_BATTERY ]; then
 		    SLEEP_TIME=$SLEEP_HIGH
 	  elif [ $CURRENT_BATTERY_LEVEL -le $VHIGH_BATTERY ]; then
@@ -54,15 +63,10 @@ while [ true ]; do
 	  else
 		    SLEEP_TIME=$SLEEP_VHIGH
 	  fi
-		echo "$(date): $CURRENT_BATTERY_LEVEL $CURRENT_BATTERY_STATUS"
-    sleep $SLEEP_TIME
-    if [ -n $NOTIFY_MSG ]; then
-       if [ -n $DISPLAY ]; then
-		      notify-send -u "critical" -i "battery-low" -c "device" "$NOTIFY_MSG"
-       else
-           #The else part is not used ever ATM
-           wall "$NOTIFY_MSG"
-       fi
+		echo "$(date): $CURRENT_BATTERY_LEVEL% $CURRENT_BATTERY_STATUS"
+    if [ -n "$NOTIFY_MSG" ]; then
+        send_message "$NOTIFY_MSG"
     fi
     NOTIFY_MSG=""
+    sleep $SLEEP_TIME
 done
