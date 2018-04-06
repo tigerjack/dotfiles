6fc918b32a073ba8c68da25dc54e22f86598e1a3
Simone Perriello <simone.perriello@protonmail.com>
2018-04-06 16:15:26 +0200

    gcalcli_reminder: improved to check connection
    Signed-off-by: Simone Perriello <simone.perriello@protonmail.com>

--- bin/x/cron/gcalcli_reminder.sh
+++ bin/x/cron/gcalcli_reminder.sh
@@ -1,8 +1,22 @@
-#!/usr/bin/bash
-
+#!/usr/bin/sh
+slt=90
 if [ -x /usr/bin/gcalcli ]; then
-  while true; do
-     /usr/bin/gcalcli --configFolder='/home/tigerjack/.config/gcalcli/' --calendar='tigerjack89@gmail.com' remind 360 'notify-send -u "critical" %s'
-    sleep 21660 # 360 min, as above
-  done &
+    echo "gcalcli detected"
+    while true; do
+        nc -z 8.8.8.8 53  >/dev/null 2>&1
+        online=$?
+        if [ $online -eq 1 ]; then
+            echo "Connection established"
+            /usr/bin/gcalcli --configFolder='/home/tigerjack/.config/gcalcli' --calendar='tigerjack89@gmail.com' remind 120 'notify-send -u "critical" %s'
+            sleep 720 # 120 min, as above
+        else
+            if [ "$slt" -ge 6000 ]; then
+                slt=6000
+            else
+                let "slt=slt*3/2"
+            fi
+            echo "Connection refused. Retrying again in $slt seconds"
+        fi
+        sleep $slt
+    done #&
 fi
