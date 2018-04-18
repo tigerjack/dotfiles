61dbf56db72ed5f6679a639c97a015b983f8ad45
Simone Perriello <simone.perriello@protonmail.com>
2018-04-18 10:04:51 +0200

    dmenu_exit: changed menu entries order
    Signed-off-by: Simone Perriello <simone.perriello@protonmail.com>

--- bin/dmenu_exit.sh
+++ bin/dmenu_exit.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-while [ "$select" != "Reboot" -a "$select" != "Shutdown" -a "$select" != "Exit" ]; do
-    select=$(echo -e 'Reboot\nShutdown\nExit' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
+while [ "$select" != "Shutdown" -a "$select" != "Reboot" -a "$select" != "Exit" ]; do
+    select=$(echo -e 'Shutdown\nReboot\nExit' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
     [ -z "$select" ] && exit 0
 done
-
-if [ "$select" = "Reboot" ]; then
-    systemctl reboot
-else if [ "$select" = "Shutdown" ]; then
-         systemctl poweroff
+if [ "$select" = "Shutdown" ]; then
+    systemctl poweroff
+else if [ "$select" = "Reboot" ]; then
+         systemctl reboot
      else if [ "$select" = "Exit" ]; then
               i3-msg exit
           fi
      fi
 fi
-
