80f01a01b91ce6b17077ea35690de7bec2f4ae83
Simone Perriello <simone.perriello@protonmail.com>
2018-04-27 10:27:18 +0200

    dmenu_exit: added Suspend option

--- bin/dmenu_exit.sh
+++ bin/dmenu_exit.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-while [ "$select" != "Shutdown" -a "$select" != "Reboot" -a "$select" != "Exit" ]; do
-    select=$(echo -e 'Shutdown\nReboot\nExit' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
+while [ "$select" != "Shutdown" -a "$select" != "Reboot" -a "$select" != "Exit" -a "$select" != "Suspend" ]; do
+    select=$(echo -e 'Shutdown\nReboot\nExit\nSuspend' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
     [ -z "$select" ] && exit 0
 done
 if [ "$select" = "Shutdown" ]; then
     systemctl poweroff
 else if [ "$select" = "Reboot" ]; then
          systemctl reboot
-     else if [ "$select" = "Exit" ]; then
-              i3-msg exit
+     else if [ "$select" = "Suspend" ]; then
+              systemctl suspend
+          else if [ "$select" = "Exit" ]; then
+                   i3-msg exit
+               fi
           fi
      fi
 fi
