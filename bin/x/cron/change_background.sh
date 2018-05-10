b7ed306d1178ec4046efa582ba3340781a364e75
Simone Perriello <simone.perriello@protonmail.com>
2018-05-10 22:00:23 +0200

    changed personal cron for background change

--- bin/x/cron/change_background.sh
+++ bin/x/cron/change_background.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true; do
-	~/.fehbg
+	feh --randomize --bg-fill /mnt/internal/Data/PersonalFolder/Pictures/NotShotwelled/Images/Astromomy/hubble_3/imagecollection/*
 	sleep 30m
 done
