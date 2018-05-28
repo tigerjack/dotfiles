1986c222dde9e66af09ffa2741f7442faa30a7a4
Simone Perriello <simone.perriello@protonmail.com>
2018-05-28 16:21:26 +0200

    brightness.sh: active monitor is now parametric

--- bin/brightness.sh
+++ bin/brightness.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
+ACTIVE_MONITOR=$(xrandr | grep -E " connected (primary )?[1-9]+" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
 CURRENT_BRIGHTNESS=$(xrandr --current --verbose | grep -m 1 'Brightness:' | cut -f2- -d:)
 CURRENT_BLUE_GAMMA=$(xrandr --current --verbose | grep -m 1 "Gamma" | cut -f4- -d:)
 DEFAULT_BRIGHTNESS="0.2"
@@ -17,20 +18,20 @@ fi
 if [[ "$1" == "+" ]] && [ $(echo "$CURRENT_BRIGHTNESS < 1.5" | bc) -eq 1 ]
 then
     echo "Going up"
-    xrandr --output "LVDS-1" --auto --brightness $(echo "$CURRENT_BRIGHTNESS + $STEP" | bc) --gamma 1:1:$(echo "scale=1; 1.0 / $CURRENT_BLUE_GAMMA" | bc)
+    xrandr --output "$ACTIVE_MONITOR" --auto --brightness $(echo "$CURRENT_BRIGHTNESS + $STEP" | bc) --gamma 1:1:$(echo "scale=1; 1.0 / $CURRENT_BLUE_GAMMA" | bc)
 elif [ "$1" == "-" ]
 then
     if [ $(echo "$CURRENT_BRIGHTNESS > 0.0" | bc) -eq 1 ]
     then
 	echo "Going down"
-	xrandr --output "LVDS-1" --brightness $(echo "$CURRENT_BRIGHTNESS - $STEP" | bc) --gamma 1:1:$(echo "scale=1; 1.0 / $CURRENT_BLUE_GAMMA" | bc)
+	xrandr --output "$ACTIVE_MONITOR" --brightness $(echo "$CURRENT_BRIGHTNESS - $STEP" | bc) --gamma 1:1:$(echo "scale=1; 1.0 / $CURRENT_BLUE_GAMMA" | bc)
     else
 	echo "Going off"
-	xrandr --output "LVDS-1" --off
+	xrandr --output "$ACTIVE_MONITOR" --off
     fi
 elif [ "$1" == "=" ] && [ $(echo "$2 > 0.0" | bc) -eq 1 ]  && [ $(echo "$2 < 1.5" | bc) -eq 1 ]
 then
-    xrandr --auto --output "LVDS-1" --brightness "$2" --gamma 1:1:$(echo "scale=1; 1.0 / $CURRENT_BLUE_GAMMA" | bc)
+    xrandr --auto --output "$ACTIVE_MONITOR" --brightness "$2" --gamma 1:1:$(echo "scale=1; 1.0 / $CURRENT_BLUE_GAMMA" | bc)
 else
     notify-send "Brightness out of range"
 fi
