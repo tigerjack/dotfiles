fa6f787129d8af8b913c8dfb3aa1075391f26b51
Simone Perriello <simone.perriello@protonmail.com>
2018-06-02 11:29:05 +0200

    dmenu_exit: added command to kill session and not only i3

--- bin/dmenu_exit.sh
+++ bin/dmenu_exit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-while [ "$select" != "Shutdown" -a "$select" != "Reboot" -a "$select" != "Exit" -a "$select" != "Suspend" ]; do
-    select=$(echo -e 'Shutdown\nReboot\nExit\nSuspend' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
+while [ "$select" != "Shutdown" -a "$select" != "Reboot" -a "$select" != "Exit"  -a "$select" != "KillSession" -a "$select" != "Suspend" ]; do
+    select=$(echo -e 'Shutdown\nReboot\nExit\nSuspend\nKillSession' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
     [ -z "$select" ] && exit 0
 done
 if [ "$select" = "Shutdown" ]; then
@@ -11,6 +11,10 @@ else if [ "$select" = "Reboot" ]; then
               systemctl suspend
           else if [ "$select" = "Exit" ]; then
                    i3-msg exit
+               else if [ "$select" = "KillSession" ]; then
+                        i3-msg exit
+		                    loginctl kill-session $XDG_SESSION_ID
+                    fi
                fi
           fi
      fi
