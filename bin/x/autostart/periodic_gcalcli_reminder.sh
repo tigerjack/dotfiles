27d90ce63195cf3496ac17e2166b60c5faaaa4b7
Simone Perriello <simone.perriello@protonmail.com>
2018-10-23 14:58:19 +0200

    gcalcli_reminder: added local bin option

--- bin/x/autostart/periodic_gcalcli_reminder.sh
+++ bin/x/autostart/periodic_gcalcli_reminder.sh
@@ -1,12 +1,12 @@
 slt=90
-if [ -x /usr/bin/gcalcli ]; then
+if [ -x /usr/bin/gcalcli ] || [ -x ~/.local/bin/gcalcli ]; then
     echo "gcalcli detected"
     while true; do
         connection_check.sh
         online=$?
         if [ $online -eq 0 ]; then
             echo "Connection established"
-            /usr/bin/gcalcli --configFolder="$XDG_CONFIG_HOME/gcalcli" --calendar='tigerjack89@gmail.com' remind 120 'notify-send -u "critical" %s'
+            gcalcli --configFolder="$XDG_CONFIG_HOME/gcalcli" --calendar='tigerjack89@gmail.com' remind 120 'notify-send -u "critical" %s'
             sleep 720 # 120 min, as above
         else
             if [ "$slt" -ge 6000 ]; then
