8ff5533e3d847e57bdc966446cbcb8910bf7646d
Simone Perriello <simone.perriello@protonmail.com>
2019-05-14 22:46:34 +0200

    Changed spawn terminal function
    * Adapted for urxvt and termite

--- bin/x/spawn_x_terminals_into_pwd.sh
+++ bin/x/spawn_x_terminals_into_pwd.sh
@@ -21,7 +21,15 @@ set -o nounset                              # Treat unset variables as an error
 # check https://stackoverflow.com/a/2013589/2326627 or https://unix.stackexchange.com/a/122848/85082 for this syntax
 n="${1:-1}"
 i=1
-while [ "$i" -le "$n" ]; do
-    urxvt -e sh -c "cd $PWD; $SHELL" &
-    i=$((i+1))
-done
+curdir=$PWD
+if [ "$TERMINAL" = "termite" ]; then
+    while [ "$i" -le "$n" ]; do
+	termite -d "$curdir" &
+	i=$((i+1))
+    done
+elif [ "$TERMINAL" = "urxvt" ]; then
+    while [ "$i" -le "$n" ]; do
+	urxvt -e sh -c "cd $curdir && bash" &
+	i=$((i+1))
+    done
+fi
