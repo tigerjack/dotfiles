28e8cf2cb5582a90cba1a3737c5b7c78f05c33de
Simone Perriello <simone.perriello@protonmail.com>
2019-09-09 10:19:25 +0200

    post_i3: added launch of .desktop files in default location

--- bin/x/post_i3_scripts.sh
+++ bin/x/post_i3_scripts.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+## Means already in desktop files
 # Polkit, started by systemctl
-lxqt-policykit-agent >"$MDIR_LOGS/startx/lxqt_policykit_agent.log$DISPLAY" 2>&1 &
+## lxqt-policykit-agent >"$MDIR_LOGS/startx/lxqt_policykit_agent.log$DISPLAY" 2>&1 &
 
 # Xscreeensaver manages screensaver, xss-lock respond to suspend/hibernate event locking screen
 xscreensaver -no-splash >"$MDIR_LOGS/startx/xscreensaver.log$DISPLAY" 2>&1 &
@@ -15,7 +16,7 @@ xbindkeys >"$MDIR_LOGS/startx/xbindkeys.log$DISPLAY" 2>&1 &
 dunst >"$MDIR_LOGS/startx/dunst.log$DISPLAY" 2>&1 &
 
 # PulseAudio
-start-pulseaudio-x11 >"$MDIR_LOGS/startx/pulseaudioX11.log$DISPLAY" 2>&1 &
+## start-pulseaudio-x11 >"$MDIR_LOGS/startx/pulseaudioX11.log$DISPLAY" 2>&1 &
 
 # simple NetworkManager front end with system tray.
 # Replaces nm-applet
@@ -25,7 +26,7 @@ nm-applet >"$MDIR_LOGS/startx/nm-applet.log$DISPLAY" 2>&1 &
 # Copyq manages clipboards.
 # I have a different configuration for each DISPLAY
 #copyq --session=${DISPLAY//:/_} >"$MDIR_LOGS/startx/copyq.log$DISPLAY" 2>&1 &
-copyq --session="${DISPLAY//:/_}" >"$MDIR_LOGS/startx/copyq.log$DISPLAY" 2>&1 &
+## copyq --session="${DISPLAY//:/_}" >"$MDIR_LOGS/startx/copyq.log$DISPLAY" 2>&1 &
 
 # Mailnag to check email accounts
 mailnag >"$MDIR_LOGS/startx/mailnag.log$DISPLAY" 2>&1 &
@@ -44,8 +45,21 @@ for file in ~/bin/x/autostart/?*.sh; do
 done
 unset file
 
+for file in /etc/xdg/autostart/?*.desktop; do
+	name="$(basename -s .desktop "$file")"
+	logfile="$MDIR_LOGS/startx/$name.log$DISPLAY"
+	(dex "$file" >"$logfile" 2>&1 & )
+done
+unset file
+
 # We should execute also all .desktop files in .config/autostart
 # to comply to XDG specifcations
+for file in ~/.config/autostart/?*.desktop; do
+	name="$(basename -s .desktop "$file")"
+	logfile="$MDIR_LOGS/startx/$name.log$DISPLAY"
+	(dex "$file" >"$logfile" 2>&1 & )
+done
+unset file
 
 # Remove previous logs
 rm "$MDIR_LINUX_DATA/logs/i3/i3-sensible-terminal.log"
