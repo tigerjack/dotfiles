cf06eecf843141475c2dcf6e186a31a9307b042c
Simone Perriello <simone.perriello@protonmail.com>
2019-09-22 21:19:46 +0200

    autostart: added first draft of drive change notifier

---
+++ bin/x/autostart/periodic_drive_changes_notifier.sh
@@ -0,0 +1,21 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: periodic_drive_changes_notifier.sh
+# 
+#         USAGE: ./periodic_drive_changes_notifier.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 09/22/2019 21:16
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+inotifywait -m -r -e modify -e delete -e create -e move "$(xdg-user-dir PUBLICSHARE)/drive/Link*"
