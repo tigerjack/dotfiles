76c055dc447ba882ddb6eab8faba1814d71e3075
Simone Perriello <simone.perriello@protonmail.com>
2020-09-06 18:45:18 +0200

    bin: modified pdf xchange wrapper

--- bin/x/pdf_xchange_wrapper.sh
+++ bin/x/pdf_xchange_wrapper.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 BASENAME=$(basename "$1")
+source ~/.global_env
 env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$BASENAME"
+
+
+# args=""
+# winpath="$(env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 winepath .)"
+# echo "$winpath"
+# winpath="Z:/home/simone"
+# for arg in $@
+# do
+#     BASENAME=$(basename "$arg")
+#     args+="$winpath/$BASENAME "
+# done
+# echo $args
+# source ~/.global_env
+# #env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$BASENAME"
+# env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$args"
