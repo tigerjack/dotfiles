6bb192e7fbe625bfe314e2db4c4ce6671af18340
Simone Perriello <simone.perriello@protonmail.com>
2020-10-04 10:59:53 +0200

    switch_theme: Added SpeedCrunch

--- bin/switch_theme.sh
+++ bin/switch_theme.sh
@@ -28,6 +28,7 @@ cfgfile_gtk3="$HOME/.config/gtk-3.0/settings.ini"
 cfgfile_i3="$HOME/.config/i3/config"
 cfgfile_qt5="$HOME/.config/qt5ct/qt5ct.conf"
 cfgfile_spt="$HOME/.config/spotify-tui/config.yml"
+cfgfile_speedcrunch="$HOME/.config/SpeedCrunch/SpeedCrunch.ini"
 
 switchto="${1:-def}"
 
@@ -43,11 +44,6 @@ elif [[ $switchto != light ]] && [[ $switchto != dark ]]; then
     exit 1
 fi
 echo "Switching to $switchto"
-### EMACS 
-# Warning: this is a toggle, so it doesn't take into account any argument, maybe change
-sed -i 's#spacemacs-light#spacemacs-dark#g;t;s#spacemacs-dark#spacemacs-light#g' "$cfgfile_spacemacs"
-## VIM
-# sed -i 's#colorscheme default#colorscheme morning#g;t;s#colorscheme morning#colorscheme default#g' "$cfgfile_vim"
 if [[ $switchto = light ]]; then
     i3style="park" 
     dmenu1="white" 
@@ -62,6 +58,7 @@ if [[ $switchto = light ]]; then
     vimbg="light" 
     spacemacsscheme="spacemacs-light" 
     spttheme="light"
+    speedcrunch="Light"
 else
     i3style="default" 
     dmenu1="black" 
@@ -76,8 +73,10 @@ else
     vimbg="dark" 
     spacemacsscheme="spacemacs-dark" 
     spttheme="dark"
+    speedcrunch="Dark"
 fi
-# The following lines should be put before the reload"
+### MAIN: gtk, qt5, i3, terminal, 
+# The following lines should be put before the reload of i3-style
 sed -i "s/set \$dmenu_options.*/set \$dmenu_options -nb $dmenu1 -nf $dmenu2 -sb $dmenu3/" "$cfgfile_i3"
 i3-style "$i3style" -o "$cfgfile_i3" --reload > /dev/null 2>&1
 termite-color-switcther "$termitestyle"
@@ -85,14 +84,19 @@ sed -i "s/^gtk-theme-name.*/gtk-theme-name=$gtktheme/" "$cfgfile_gtk2"
 sed -i "s/^gtk-theme-name.*/gtk-theme-name=$rofitheme/" "$cfgfile_gtk3"
 sed -i "s/^gtk-application-prefer-dark-theme.*/gtk-application-prefer-dark-theme=$dark/" "$cfgfile_gtk3"
 sed -i "s;^color_scheme_path.*;color_scheme_path=/usr/share/qt5ct/colors/$qt5colors;" "$cfgfile_qt5"
+
+### ROFI
 sed -i "s;^rofi.theme.*;rofi.theme: $rofitheme;" "$cfgfile_rofi"
+### VIM
 sed -i "s/^colorscheme.*/colorscheme $vimscheme/" "$cfgfile_vim"
 sed -i "s/^set background.*/set background=$vimbg/" "$cfgfile_vim"
-
 ### VIM SERVER
 for i in $(vim --serverlist); do 
     vim --servername "$i" --remote-send ":colorscheme $vimscheme<CR>"
 done
+### EMACS 
+# WARN: this is a toggle, so it doesn't take into account any argument, maybe change
+sed -i 's#spacemacs-light#spacemacs-dark#g;t;s#spacemacs-dark#spacemacs-light#g' "$cfgfile_spacemacs"
 ### EMACS SERVER
 bla=$(pgrep -f "emacs --daemon")
 if [ -n "$bla" ]; then
@@ -102,12 +106,16 @@ if [ -n "$bla" ]; then
 	echo "Unable to change theme in spacemacs"
     fi
 fi
-
 ### SPOTIFY-TUI
 # WARN: This overwrite any personalized configuration in yaml
 # TODO: A parser of config that preserves non-theme lines
 ln -sf "$HOME/.config/spotify-tui/theme_$spttheme.yml" "$cfgfile_spt"
+### SPEEDCRUNCH
+# WARN: doesn't work if already active; on exit, it overwrites changes
+sed -i "s;ColorSchemeName.*;ColorSchemeName=Solarized $speedcrunch;" "$cfgfile_speedcrunch"
 
 # Still missing 
 # - Firefox: changes to user.js aren't loaded automatically; you can use a plugin to rotate
 # - Thunderbird: no easy switch 
+# - CopyQ: theme not changeable easily
+# - GoldenDict
