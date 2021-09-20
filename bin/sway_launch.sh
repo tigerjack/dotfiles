9c37171aafe95bf5ad8f961b054d3ff96a0bd142
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2021-09-20 16:28:58 +0200

    sway_launch: changed script

--- bin/sway_launch.sh
+++ bin/sway_launch.sh
@@ -18,6 +18,10 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
+source ~/.env_global
 source ~/.env_gui
-exec /usr/bin/sway
+# exec /usr/bin/sway
+# D-Bus session bus is started automatically, but if you want to have multiple simultaneous sessions for the same user on the same machine, you may want to create own bus for each session and therefore explicitly invoke dbus-run-session
+# exec dbus-run-session /usr/bin/sway
+exec dbus-run-session /usr/bin/sway -d 2>~/sway.log
 
