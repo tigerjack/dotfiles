61278803d878bb37fc98148692cc0f8478682086
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2021-12-28 16:09:09 +0100

    dmenu_exit: switch to wofi

--- bin/x/dmenu_exit.sh
+++ bin/x/dmenu_exit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 while [ "$select" != "Shutdown" ] && [ "$select" != "Reboot" ] && [ "$select" != "Exit" ] && [ "$select" != "KillSession" ] && [ "$select" != "Suspend" ]; do
-    select=$(echo -e 'Shutdown\nReboot\nExit\nSuspend\nKillSession' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
+    #select=$(echo -e 'Shutdown\nReboot\nExit\nSuspend\nKillSession' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "Select your choice")
+    select=$(echo -e 'Shutdown\nReboot\nExit\nSuspend\nKillSession' | wofi --show=dmenu -i -p "Select your choice")
     [ -z "$select" ] && exit 0
 done
 if [ "$select" = "Shutdown" ]; then
@@ -10,10 +11,8 @@ elif [ "$select" = "Reboot" ]; then
 elif [ "$select" = "Suspend" ]; then
     systemctl suspend
 elif [ "$select" = "Exit" ]; then
-    # i3-msg exit
     swaymsg exit
 elif [ "$select" = "KillSession" ]; then
-    # i3-msg exit
     swaymsg exit
     #loginctl kill-session $XDG_SESSION_ID
     loginctl terminate-session "$XDG_SESSION_ID"
