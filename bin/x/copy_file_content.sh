be1e193fe9296c4101b4b8a6c212e5f76bea8bcd
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-01-06 13:44:52 +0100

    Removed useless GUI bins and backup files

--- bin/x/copy_file_content.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#echo "$1"
-t=$(file -b --mime-type "$1")
-#echo $t
-#cat "$1" | xclip -selection c -t "$t"
-xclip -selection c -t "$t" -i "$1"
-#xclip -selection c -t "$t" < "$1"
-#copyq write $t - < $1 && copyq select 0
--- bin/x/deskopen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-readonly to_exec="$(grep '^Exec' "$1" | tail -1 | sed 's/^Exec=//' | sed 's/%.//' | sed 's/^"//g' | sed 's/" *$//g')"
-echo "$to_exec"
-$to_exec &
