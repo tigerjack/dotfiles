99f9c02d9a843d362537dbe858612e5229060dab
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-01-07 09:51:18 +0100

    sway: changed background
    Now last-used background is used as the entry wallpaper

--- bin/x/periodic_change_background.sh
+++ bin/x/periodic_change_background.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 while true; do
-	echo "Background changed"
-	# feh --randomize --bg-fill -r "$(xdg-user-dir PICTURES)/NotShotwelled/Images/Astronomy/"*
-	# feh --randomize --bg-fill $(xdg-user-dir PICTURES)/NotShotwelled/Images/Astromomy/hubble_3/imagecollection/*
-	setrandom -v "$(xdg-user-dir PICTURES)/NotShotwelled/Backgrounds/esahubble-top100"
-	sleep 30m
+    # We sleep first bcz in the beginning the background is set to the old wallpaper
+    sleep 30m
+    # echo "Changing background"
+    # feh --randomize --bg-fill -r "$(xdg-user-dir PICTURES)/NotShotwelled/Images/Astronomy/"*
+    # feh --randomize --bg-fill $(xdg-user-dir PICTURES)/NotShotwelled/Images/Astromomy/hubble_3/imagecollection/*
+    # execute set random and take the command executed, which is in the form `swaymsg output * bg ...`
+    setrandom -v "$(xdg-user-dir PICTURES)/NotShotwelled/Backgrounds/esahubble-top100" | grep swaymsg | cut -d' ' -f2- > ~/.config/sway/conf.d/wallpaper.conf
 done
