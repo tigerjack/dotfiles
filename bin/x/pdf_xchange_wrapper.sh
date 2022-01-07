95299956aae13d877537f4b9036b552fb160cd2d
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-01-07 14:04:31 +0100

    Mod PDF XChange Editor programs
    - Add wrappers to desktop files
    - Fix argument

--- bin/x/pdf_xchange_wrapper.sh
+++ bin/x/pdf_xchange_wrapper.sh
@@ -2,21 +2,7 @@
 # BASENAME=$(basename "$1")
 source ~/.env_global
 source ~/.bash.d/bash_aliases
+filename="z:${1//\//\\}"
 # wn64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$BASENAME"
-wn64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" $1
+wn64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$filename"
 #env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$BASENAME"
-
-
-# args=""
-# winpath="$(env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 winepath .)"
-# echo "$winpath"
-# winpath="Z:/home/simone"
-# for arg in $@
-# do
-#     BASENAME=$(basename "$arg")
-#     args+="$winpath/$BASENAME "
-# done
-# echo $args
-# source ~/.env_global
-# #env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$BASENAME"
-# env WINEPREFIX="$MDIR_LINUX_DATA/wine64/" env WINEARCH=win64 /usr/bin/wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$args"
--- bin/x/pdf_xchange_wrapper2.sh
+++ bin/x/pdf_xchange_wrapper2.sh
@@ -2,5 +2,7 @@
 # BASENAME=$(basename "$1")
 source ~/.env_global
 source ~/.bash.d/bash_aliases
+filename="z:${1//\//\\}"
 # wn64 wine "C:/windows/command/start.exe" /Unix "/mnt/internal/LinuxData/wine64/dosdevices/c:/users/simone/Start Menu/Programs/Tracker Software/PDF-XChange Editor.lnk" "$BASENAME"
-wn64 wine start.exe /Unix "/mnt/internal/LinuxData/wine64/dosdevices/c:/users/simone/Start Menu/Programs/Tracker Software/PDF-XChange Editor.lnk" "$1"
+# wn64 wine start.exe /Unix "/mnt/internal/LinuxData/wine64/dosdevices/c:/users/simone/Start Menu/Programs/Tracker Software/PDF-XChange Editor.lnk" "$1"
+wn64 wine start.exe /Unix "/mnt/internal/LinuxData/wine64/dosdevices/c:/Program Files/Tracker Software/PDF Editor/PDFXEdit.exe" "$filename"
