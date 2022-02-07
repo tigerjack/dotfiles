83ee6a4d2784fa063a2b8fb43fe735e2b66fff79
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-02-07 11:40:34 +0100

    sway: switch to develop version

--- bin/sway_launch.sh
+++ bin/sway_launch.sh
@@ -20,7 +20,8 @@
 set -o nounset                              # Treat unset variables as an error
 source ~/.env_global
 source ~/.env_gui
-exec /usr/bin/sway
+# exec /usr/bin/sway
+exec /mnt/internal/LinuxData/online_repos/git/forked/sway/build/sway/sway -d
 # D-Bus session bus is started automatically, but if you want to have multiple simultaneous sessions for the same user on the same machine, you may want to create own bus for each session and therefore explicitly invoke dbus-run-session. See https://www.reddit.com/r/swaywm/comments/csb5w8/how_to_start_a_local_dbus_session/ and https://www.reddit.com/r/swaywm/comments/ibainf/sway_xdg_dbus_session_pulseaudio_systemd_etc_need/
 # However, it can create problems https://bbs.archlinux.org/viewtopic.php?pid=2013344
 # exec dbus-run-session /usr/bin/sway
