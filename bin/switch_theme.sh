78316454569b88675191f2741edee31c2fac12c6
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2022-09-09 07:12:03 +0200

    switch_theme: minors

--- bin/switch_theme.sh
+++ bin/switch_theme.sh
@@ -57,6 +57,7 @@ if [[ $switchto = light ]]; then
     kittystyle="Pencil Light"
     # termitestyle="atom-one-light" 
     alacrittystyle="Google.light"
+    osc411style="tango-light"
     gtktheme="\"Arc\"" 
     rofitheme="Arc" 
     dark="false" 
@@ -80,6 +81,7 @@ else
     dmenu2="white" 
     dmenu3="red" 
     alacrittystyle="Google.dark"
+    osc411style="tango-dark"
     #kittystyle="Misterioso"
     kittystyle="Pencil Dark"
     # termitestyle="default" 
@@ -102,26 +104,47 @@ else
 fi
 ### MAIN: gtk, qt5, i3, terminal, 
 # The following lines should be put before the reload of i3-style
-sed -i "s/set \$dmenu_options.*/set \$dmenu_options -nb $dmenu1 -nf $dmenu2 -sb $dmenu3/" "$cfgfile_i3"
-i3-style "$i3style" -o "$cfgfile_i3" --reload > /dev/null 2>&1
+# sed -i "s/set \$dmenu_options.*/set \$dmenu_options -nb $dmenu1 -nf $dmenu2 -sb $dmenu3/" "$cfgfile_i3"
+# i3-style "$i3style" -o "$cfgfile_i3" --reload > /dev/null 2>&1
+# echo "i3-style done"
+
 # ALACRITTY
+# OFF
 # https://github.com/rajasegar/alacritty-themes
 # https://github.com/rajasegar/alacritty-themes/tree/master/themes
-alacritty-themes "$alacrittystyle"
+# alacritty-themes "$alacrittystyle"
+
 # KITTY
 kitty +kitten themes --reload-in=all  "$kittystyle"
+echo "kitty done"
+
 # TERMITE you should first install termite-color-switcher
 # OFF
 # termite-color-switcher "$termitestyle"
+# ALL OSC 4/11 Terminal emulators via theme.sh and bashrc; works only on new instances
+# OFF
+# theme.sh "$osc411style"
+
 # Others
 sed -i "s/^gtk-theme-name.*/gtk-theme-name=$gtktheme/" "$cfgfile_gtk2"
-sed -i "s/^gtk-theme-name.*/gtk-theme-name=$rofitheme/" "$cfgfile_gtk3"
 sed -i "s/^gtk-application-prefer-dark-theme.*/gtk-application-prefer-dark-theme=$dark/" "$cfgfile_gtk3"
 sed -i "s;^color_scheme_path.*;color_scheme_path=/usr/share/qt5ct/colors/$qt5colors;" "$cfgfile_qt5"
 sed -i "s;^icon_theme.*;icon_theme=$qt5icons;" "$cfgfile_qt5"
+echo "gtk, qt5 done"
+
+# This work for Firefox, Wofi and Thunderbird; however, the latter doesn't work
+# see here https://wiki.archlinux.org/title/Dark_mode_switching#Tools
+gsettings set org.gnome.desktop.interface gtk-theme "$gtktheme"
+echo "GTK through gsettings done"
+
+qt5ct-refresh
+echo "qt5ct-refresh done"
 
 ### ROFI
 # sed -i "s;^rofi.theme.*;rofi.theme: $rofitheme;" "$cfgfile_rofi"
+# sed -i "s/^gtk-theme-name.*/gtk-theme-name=$rofitheme/" "$cfgfile_rofi"
+# echo "rofi done"
+
 ### VIM
 # sed -i "s/^colorscheme.*/colorscheme $vimscheme/" "$cfgfile_vim"
 # sed -i "s/\scolorscheme.*/ colorscheme $vimscheme/" "$cfgfile_vim"
@@ -129,8 +152,11 @@ sed -i "s;^icon_theme.*;icon_theme=$qt5icons;" "$cfgfile_qt5"
 sed -i "s/^set background.*/set background=$vimbg/" "$cfgfile_vim"
 ### VIM SERVER, this requires a vim installed with gui support
 for i in $(vim --serverlist); do 
+    echo "vim server(s) running"
     vim --servername "$i" --remote-send ":colorscheme $vimscheme<CR>"
 done
+echo "vim done"
+
 ### EMACS 
 # WARN: this is a toggle, so it doesn't take into account any argument, maybe change
 sed -i 's#spacemacs-light#spacemacs-dark#g;t;s#spacemacs-dark#spacemacs-light#g' "$cfgfile_spacemacs"
@@ -143,38 +169,39 @@ if [ -n "$bla" ]; then
 	echo "Unable to change theme in spacemacs"
     fi
 fi
+echo "emacs done"
+
 ### TASKWARRIOR
 sed -i "s;^include.*;include $taskwarriortheme;" "$cfgfile_taskwarrior"
+echo "taskwarrior done"
+
 ### RANGER
 sed -i "s/^set colorscheme .*/set colorscheme=$rangerscheme/" "$cfgfile_ranger"
+echo "ranger done"
 
 ### SPOTIFY-TUI
 # WARN: This overwrite any personalized configuration in yaml
 # TODO: A parser of config that preserves non-theme lines
 ln -sf "$HOME/.config/spotify-tui/theme_$spttheme.yml" "$cfgfile_spt"
+echo "spotify tui done"
+
 ### SPEEDCRUNCH
 # WARN: doesn't work if already active; on exit, it overwrites changes
 sed -i "s;ColorSchemeName.*;ColorSchemeName=Solarized $speedcrunch;" "$cfgfile_speedcrunch"
+echo "speedcrunch done"
 
 ### ZATHURA
 for i in $(pidof zathura); do
     dbus-send --type="method_call" --dest=org.pwmt.zathura.PID-"$i" /org/pwmt/zathura org.pwmt.zathura.ExecuteCommand string:"set recolor"
 done
+echo "zathura done"
 
-# This work for Firefox, Wofi and Thunderbird; however, the latter doesn't work
-# see here https://wiki.archlinux.org/title/Dark_mode_switching#Tools
-echo "GTK through gsettings"
-gsettings set org.gnome.desktop.interface gtk-theme "$gtktheme"
-
-echo "qt5ct-refresh"
-qt5ct-refresh
-
-echo "mako"
 makoctl set-mode "$makomode"
+echo "mako done"
 
-echo "spotify"
-spicetify config current_theme "$spotifytheme" color_scheme "$spotifytheme"
+spicetify config current_theme "$spotifytheme" color_scheme "$spotifyscheme"
 spicetify apply
+echo "spicety for spotify"
 
 # cht.sh
 sed -i "s;style=.*;style=$chtshstyle\";" "$cfgfile_chtsh"
