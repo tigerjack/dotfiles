2bc9662a727bf3fc2d82f6be78cbd3108864c838
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-02-05 18:04:53 +0100

    MOD pdf_xchange_wrapper10.sh path

--- bin/x/pdf_xchange_wrapper10.sh
+++ bin/x/pdf_xchange_wrapper10.sh
@@ -5,4 +5,4 @@ filename=$(basename "$1")
 # The alternative 2 is converting the linux to the windows path
 # filename=$(wn64a winepath -w "$1")
 # wine64c wine64 start.exe /Unix "/mnt/internal/LinuxData/wine64c/dosdevices/c:/Program Files/Tracker Software/PDF Editor/PDFXEdit.exe" "$filename"
-WINEARCH=win64 WINEPREFIX=$MDIR_LINUX_DATA/wine64c wine64 start.exe /Unix "/mnt/internal/LinuxData/wine64c/dosdevices/c:/Program Files/Tracker Software/PDF Editor/PDFXEdit.exe" "$filename"
+WINEARCH=win64 WINEPREFIX=$MDIR_LINUX_DATA/wine64c wine64 start.exe /Unix "$MDIR_LINUX_DATA/wine64c/dosdevices/c:/Program Files/Tracker Software/PDF Editor/PDFXEdit.exe" "$filename"
