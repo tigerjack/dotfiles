9f1642d78f78d1a1700208cddf39a0528c64bc58
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-02-11 16:44:45 +0100

    MOD gitignore

--- Readme.md
+++ Readme.md
@@ -18,7 +18,7 @@ It will
 0. Set hostname if not already specified inside /etc/hostname
 1. Set graphic cards in .env_gui
 2. Set relevant dirs in .env_global
-3. Execute generate_from_global_env
+3. Execute generate_config
 4. Execute switch_theme.sh. Note that theme lines are untracked from git using this method https://stackoverflow.com/questions/6557467/can-git-ignore-a-specific-line
 5. Enable systemd units. For example
 ```bash
---
+++ bin/generate_config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: generate_config.sh
+# 
+#         USAGE: ./generate_config.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 11/02/2025 19:49
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+bin/generate_from_global_env.sh
+
+declare -a arr=("$XDG_CONFIG_HOME/waybar/config");
+
+# Set user directories
+for i in "${arr[@]}"; do
+    ln -s "$i.$HOSTNAME" "$i"
+done
+
+
