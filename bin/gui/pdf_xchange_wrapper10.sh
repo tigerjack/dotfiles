ff023f618ac2b7a77d42fedbba50e88841b7c1c8
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-02-27 09:19:32 +0100

    FIX pdf xchange scripts

--- bin/gui/pdf_xchange_wrapper10.sh
+++ bin/gui/pdf_xchange_wrapper10.sh
@@ -1,8 +1,9 @@
 source ~/.env_global
-source ~/.bash.d/bash_aliases
-# wine should work well with namefile only, without absolute path
-filename=$(basename "$1")
-# The alternative 2 is converting the linux to the windows path
-# filename=$(wn64a winepath -w "$1")
-# wine64c wine64 start.exe /Unix "/mnt/internal/LinuxData/wine64c/dosdevices/c:/Program Files/Tracker Software/PDF Editor/PDFXEdit.exe" "$filename"
-WINEARCH=win64 WINEPREFIX=$MDIR_LINUX_DATA/wine64c wine64 start.exe /Unix "$MDIR_LINUX_DATA/wine64c/dosdevices/c:/Program Files/Tracker Software/PDF Editor/PDFXEdit.exe" "$filename"
+export WINEPREFIX="$MDIR_LINUX_DATA/wine64c"
+export WINEARCH=win64
+
+# Convert Unix path to Windows path
+winpath=$(winepath -w "$1")
+
+# Run PDF-XChange Editor
+wine start.exe /Unix "$WINEPREFIX/dosdevices/c:/Program Files/Tracker Software/PDF Editor/PDFXEdit.exe" "$winpath"
--- bin/gui/pdf_xchange_wrapper6_porta.sh
+++ bin/gui/pdf_xchange_wrapper6_porta.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 source ~/.env_global
-source ~/.bash.d/bash_aliases
-# Warning. It seems that wine has problems
-# with directory having a . in their name
-#
-# wine should work well with namefile only, without absolute path
-filename=$(basename "$1")
-# The alternative 2 is converting the linux to the windows path
-# filename=$(wn64a winepath -w "$1")
-#
-echo "$filename"
-wn64a wine64 "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$filename"
+export WINEPREFIX="$MDIR_LINUX_DATA/wine64a"
+export WINEARCH=win64
+
+winpath=$(winepath -w "$1")
+wine "$MDIR_STANDALONE_SW/Windows/Reader/PDF-XChange Editor Plus [6.0.317.1] [crack]/PDFXEdit.exe" "$winpath"
